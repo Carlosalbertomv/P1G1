dc5660155d108e7f6811df4454153a081930c896
Carlosalbertomv <132417628+Carlosalbertomv@users.noreply.github.com>
2023-05-16 20:27:17 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,14 @@
+
+function factorialN(){
+var N = prompt ("Ingrese N", 3)
+var i = 1
+var resp = 1
+if(N >= 0){
+if (N == 0){
+    alert("el factorial de N es " + resp)
+    }
+}else{
+    alert("no factorial de un negativo")
+}
+
+}
