79de87389e05c86dc56d501a5abe0dbb7d0e9a7f
Carlosalbertomv <132417628+Carlosalbertomv@users.noreply.github.com>
2023-05-18 21:50:58 -0400

    Add files via upload

--- DF code/code.js
+++ DF code/code.js
@@ -83,4 +83,70 @@ function mostrarDigN() {
     
 }
 
+function calcularTrapecio(){
+    var h = parseInt(prompt("Ingrese h", 10))
+    var b = parseInt(prompt("Ingrese h", 5))
+    var B = parseInt(prompt("Ingrese h", 15))
+    var a = parseInt(prompt("Ingrese h", 10))
+    var c = parseInt(prompt("Ingrese h", 7))
+    var area = 0 
+    var perimetro = 0
+    area = ((b + B)/2) * h
+    perimetro = a + b + B + C
+    alert("El area es :"  + area )
+    alert("El perimetro es :"  + perimetro )
+}
+
+function determinarNesParImpar(){
+    var N = parseInt(prompt("Ingrese N"))
+    if ((N % 2) == 0){
+        alert(N + " es par.")
+    } else {
+        alert(N + " es impar.")
+    }
+}
+
+function ifNormal() {
+    var N = parseInt(prompt("Ingrese N"))
+    if (N > 5) {
+        N = 5
+    } else {
+        N = 5
+        alert(N)
+    }
+}
+
+function ifAbreviado() {
+    var N = parseInt(prompt("Ingrese N"))
+       (N > 5) {
+        N = 10
+    } else {
+        N = 1
+        alert (N)
+    }
+}
+/*
+    var N = parseInt(prompt("Ingrese N", 9478324))
+    var cantidad = 0
+    while (N != 0) {
+        N = parseInt(N/ 10)
+        cantidad = cantidad + 1
+    }
+    alert("digitos: " + cantidad)
+*/
+
+function obtenerCantidadDigN_FOR() {
+    var N = parseInt(prompt("Ingrese N", 9478324))
+    var cantidad
+    for (cantidad = 0; N !=  0; cantidad++) {
+        N = parseInt(N / 10)
+    }
+    alert("digitos: " + cantidad)
+}
+        
+    
+
+
+
+
 
