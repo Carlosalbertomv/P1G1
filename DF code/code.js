d5ac31c4ad494d74c1e711841ec3dada8f44baff
Carlosalbertomv <132417628+Carlosalbertomv@users.noreply.github.com>
2023-05-25 21:36:50 -0400

    Add files via upload

--- DF code/code.js
+++ DF code/code.js
@@ -1,339 +1,501 @@
+//////////// variables globales
+
+var A = 0 
+var operacion = ""
+
+
+//////////// FUNCIONES
 
 function factorialN() {
-    var N = prompt("Ingrese N", 3)
-    var i = 1
-    var resp = 1
-    if (N >= 0) {
-        if (N == 0) {
-            alert("El factorial de N es " + resp)
-        } else {
-            while (N >= i) {
-                resp = resp * i
-                i = i + 1
-            }
-            alert("Factorial de N es: " + resp)
-        }
-    } else {
-        alert("No factorial de un negativo")
-    }
-}
+    var N = prompt("Ingrese un valor:", 3);
+    var i = 1;
+    var resp = 1;
 
-function calcularFactorial(N) {
-    //var N = prompt("Ingrese N", 3)
-    var i = 1
-    var resp = 1
     if (N >= 0) {
         if (N == 0) {
-            //alert("El factorial de N es " + resp)
-            return resp
+            alert("El factorial de " + N + " es " + resp + ".");
         } else {
-            while (N >= i) {
-                resp = resp * i
-                i = i + 1
+            while (i <= N) {
+                resp *= i;
+                i += 1;
             }
-            //alert("Factorial de N es: " + resp)
-            return resp
+            alert("El factorial de " + N + " es " + resp + ".");
         }
     } else {
-        alert("No factorial de un negativo")
+        alert("No existe el factorial de un negativo.");
     }
 }
 
 function mayorAyB() {
-    var A = parseInt(prompt("Ingrese A", 10))
-    var B = parseInt(prompt("Ingrese B", 7))
+    var A = prompt("Ingrese A", 10);
+    A = parseInt(A);
+    var B = prompt("Ingrese B", 7);
+    B = parseInt(B);
+
     if (A == B) {
-        alert("son iguales")
+        alert("Los dos son iguales.");
     } else {
         if (A > B) {
-            alert("el mayor es A: " + A)
+            alert("El mayor es A: " + A);
         } else {
-            alert("el mayor es B: " + B)
+            alert("El mayor es B: " + B);
         }
     }
 }
 
 function menorAyB() {
-    var A = parseInt(prompt("Ingrese A", 10))
-    var B = parseInt(prompt("Ingrese B", 7))
+    var A = prompt("Ingrese A", 10);
+    A = parseInt(A);
+    var B = prompt("Ingrese B", 7);
+    B = parseInt(B);
+
     if (A == B) {
-        alert("son iguales")
+        alert("Los dos son iguales.");
     } else {
         if (A > B) {
-            alert("el menor es B: " + B)
+            alert("El menor es B: " + B);
         } else {
-            alert("el menor es A: " + A)
+            alert("El menor es A: " + A);
         }
     }
 }
 
 function obtenerDiv() {
-    var A = parseInt(prompt("Ingrese A", 10))
-    var B = parseInt(prompt("Ingrese B", 7))
-    var resultado=0
-    resultado=parseInt(A/B)
-    alert(resultado)
+    var A = parseInt(prompt("Ingrese A:", 10));
+    var B = parseInt(prompt("Ingrese B:", 7));
+    var resultado = ~~(A / B);
+
+    alert(resultado);
 }
 
 function obtenerMod() {
-    var A = parseInt(prompt("Ingrese A", 10))
-    var B = parseInt(prompt("Ingrese B", 7))
-    var resultado=0
-    resultado=parseInt(A%B)
-    alert(resultado)
+    var A = parseInt(prompt("Ingrese A:", 10));
+    var B = parseInt(prompt("Ingrese B:", 7));
+    var resultado = A % B;
+
+    alert(resultado);
 }
 
-function obtenerMod() {
-    var A = parseInt(prompt("Ingrese A", 10))
-    var B = parseInt(prompt("Ingrese B", 7))
-    var resultado=0
-    resultado=parseInt (A%B)
-    alert(resultado)
-}
+function digitosN() {
+    var N = parseInt(prompt("Ingresar N", 3));
+    var resp = N;
+    var cantidad = 1;
 
-function mostrarDigN() {
-    var N = parseInt(prompt("Ingrese N", 9478324))
-    var dig = 0
-    while (N != 0) {
-        dig = parseInt (N % 10)
-        if (dig == 3){
-            alert("Existe el dig 3")
-        }
-        N = parseInt  ()
-    }
-    
-}
-
-function calcularTrapecio(){
-    var h = parseInt(prompt("Ingrese h", 10))
-    var b = parseInt(prompt("Ingrese h", 5))
-    var B = parseInt(prompt("Ingrese h", 15))
-    var a = parseInt(prompt("Ingrese h", 10))
-    var c = parseInt(prompt("Ingrese h", 7))
-    var area = 0 
-    var perimetro = 0
-    area = ((b + B)/2) * h
-    perimetro = a + b + B + C
-    alert("El area es :"  + area )
-    alert("El perimetro es :"  + perimetro )
-}
-
-function determinarNesParImpar(){
-    var N = parseInt(prompt("Ingrese N"))
-    if ((N % 2) == 0){
-        alert(N + " es par.")
+    if (N == 0) {
+        cantidad = 1;
+        alert(cantidad + " dígitos");
     } else {
-        alert(N + " es impar.")
+        while (N != 0) {
+            N = parseInt(N / 10);
+            cantidad += 1;
+        }
+        alert(cantidad + " dígitos");
     }
+}
+
+function retornarDigitosN() {
+    var N = parseInt(prompt("Ingresar N", 3));
+    var dig = 0;
+
+    if (N == 0) {
+        dig = N;
+        alert(dig);
+    } else {
+        while (N != 0) {
+            dig = N % 10;
+            alert(dig);
+            N = parseInt(N / 10);
+        }
+    }
+}
+
+function mostrarTres() {
+    var N = parseInt(prompt("Ingresar N", 3));
+    var dig = 0;
+
+    while (N != 0) {
+        dig = N % 10;
+        if (dig == 3) {
+            alert("Existe el número 3");
+            break
+        }
+        N = parseInt(N / 10);
+    }
+}
+
+function areaPerimetroTrapecio() {
+    var h = parseInt(prompt("Ingrese h:", 10));
+    var b = parseInt(prompt("Ingrese b:", 5));
+    var B = parseInt(prompt("Ingrese B:", 15));
+    var a = parseInt(prompt("Ingrese a:", 10));
+    var c = parseInt(prompt("Ingrese c:", 7));
+
+    var Area = 0, Perimetro = 0;
+
+    Area = ((b + B) / 2) * h;
+    Perimetro = a + b + B + c;
+
+    alert("El área es: " + Area);
+    alert("El perímetro es: " + Perimetro);
+}
+
+function NParOImpar() {
+    /* If normal
+
+    var N = parseInt(prompt("Ingrese N:", 115));
+
+    if ((N % 2) == 0) {
+        alert(N + " es par.");
+    } else {
+        alert(N + " es impar.");
+    }
+
+    */
+   
+    // If abreviado
+
+    var N = parseInt(prompt("Ingrese N:", 115));
+    (N % 2) == 0 ? 10 : 1;
 }
 
 function ifNormal() {
-    var N = parseInt(prompt("Ingrese N"))
+    var N = parseInt(prompt("Ingrese N:", 115));
+
     if (N > 5) {
-        N = 5
+        N = 10;
     } else {
-        N = 5
-        alert(N)
+        N = 1;
     }
+    alert(N)
 }
 
 function ifAbreviado() {
-    var N = parseInt(prompt("Ingrese N"))
-       (N > 5) 
-        N = 10
-    } 
-        N = 1
-        alert (N)
-    
-
-/*
-    var N = parseInt(prompt("Ingrese N", 9478324))
-    var cantidad = 0
-    while (N != 0) {
-        N = parseInt(N/ 10)
-        cantidad = cantidad + 1
-    }
-    alert("digitos: " + cantidad)
-*/
-
-function obtenerCantidadDigN_FOR() {
-    var N = parseInt(prompt("Ingrese N", 9478324))
-    var cantidad
-    for (cantidad = 0; N !=  0; cantidad++) {
-        N = parseInt(N / 10)
-    }
-    alert("digitos: " + cantidad)
+    var N = parseInt(prompt("Ingrese N:", 115));
+    N = (N > 5) ? 10 : 1;
+    alert(N);
 }
-        
-    function NmultiploDeM() {
-        var N = parseInt(prompt("Ingrese N: ", 10))
-        var M = parseInt(prompt("Ingrese M: ", 5))
 
-        if ((N % M) == 0) {
-            alert(N + " es multiplo de " + M)
+function digitosNFOR() {
+    var N = parseInt(prompt("Ingresar N", 3));
+
+    if (N == 0) {
+        alert(1 + " dígitos");
+    } else {
+        for (var cantidad = 0; N != 0; cantidad += 1) {
+            N = parseInt(N / 10);
+        }
+        alert(cantidad + " dígitos");
+    }
+}
+
+// Práctico Segundo Parcial
+
+
+
+function digitosImpares() {
+    var N = parseInt(prompt("Ingresar N", 3));
+    var dig = 0;
+
+    while (N != 0) {
+        dig = N % 10;
+        if (dig % 2 != 0) {
+            alert(dig)
+        }
+        N = parseInt(N / 10);
+    }
+}
+
+function sumatoria1a10() {
+    var sumatoria = 0;
+    var i = 1;
+
+    while (i <= 10) {
+        sumatoria += i;
+        i++;
+    }
+    alert("sumatoria del 1 al 10: " + sumatoria);
+}
+
+function areaVolumenCilindro() {
+    var r = parseInt(prompt("Ingrese el radio:", 10));
+    var h = parseInt(prompt("Ingrese la altura:", 5));
+
+    var Area = 0, Volumen = 0;
+
+    Area = 2 * Math.PI * r * (r + h);
+    Volumen = Math.PI * Math.pow(r, 2) * h;
+
+    alert("El área es: " + Area);
+    alert("El perímetro es: " + Volumen);
+}
+
+function mostrarNumPosNeg() {
+    var N = parseInt(prompt("Ingresar N", 3));
+
+    if (N == 0) {
+        alert("Neutro");
+    } else {
+        if (N > 0) {
+            alert("Positivo");
         } else {
-            alert(N + " no es multiplo de " + M)
+            alert("Negativo");
         }
     }
+}
 
-function sumaDigMultiplos3deN() {
-    var N = parseInt(prompt("Ingrese N: ", 23462314))
-    var sum = 0
-    var dig = 0
+function mostrarNumMenor() {
+    var A = parseInt(prompt("Ingrese A:", 10));
+    var B = parseInt(prompt("Ingrese B:", 7));
+
+    if (A > B) {
+        alert(B + " es menor");
+    } else {
+        alert(A + " es menor");
+    }
+}
+
+function NmultiploDeM() {
+    var N = parseInt(prompt("Ingrese N:", 10));
+    var M = parseInt(prompt("Ingrese M:", 5));
+
+    if ((N % M) == 0) {
+        alert(N + " es múltiplo de " + M);
+    } else {
+        alert(N + " no es múltiplo de " + M);
+    }
+}
+
+function sumatoriaMultiplosTres() {
+    var N = parseInt(prompt("Ingrese N:", 10));
+    var sum = 0, dig = 0;
+    
     while (N != 0) {
-        dig = parseInt(N % 10)
-        alert(dig)
+        dig = N % 10;
         if ((dig % 3) == 0) {
-            sum = sum + dig
-        } else {
-
+            sum += dig;
         }
+        N = parseInt(N / 10);
     }
+
+    alert(sum);
 }
 
-function sumarNrosEntreAyB() {
-    var A = parseInt(prompt("Ingrese A: ", 3))
-    var B = parseInt(prompt("Ingrese B: ", 9))
-    var suma = 0
-
+function sumAyBInclusivo() {
+    var A = parseInt(prompt("Ingrese A:", 3));
+    var B = parseInt(prompt("Ingrese B:", 9));
+    var suma = 0;
     
-} 
-
-function promedioNdeNotas() {
-    var N = parseInt(prompt("Ingrese N: ", 15))
-    var sumaNota = 0
-    var i = 0
-    while(N > i){
-        i = i + 1
-        var nota = parseInt(prompt("Ingrese Nota: ", 80))
-        sumaNota = sumaNota + nota
+    while (A <= B) {
+        suma = suma + A;
+        A = A + 1;
     }
-    alert(sumaNota/N)
+    
+    alert(suma);
 }
 
-var myVariableGlobal = "Hola mundo"
+function promedioNnotas() {
+    var N = parseInt(prompt("Ingrese N:", 15));
+    var suma = 0;
+    var i = 0;
+    
+    while (i < N) {
+        var nota = parseInt(prompt("Ingrese la nota:", 80));
+        suma = suma + nota;
+        i = i + 1;
+    }
+    
+    suma = suma / N;
+    alert(suma);
+}
+
+var myVarGlobal = "Hola Mundo";
 
 function obtenerNombreCompleto(nombre, apellido) {
-    var nombreCompleto = nombre + " " + apellido
-    return nombreCompleto
+    var nombreCompleto = nombre + " " + apellido + " " + myVarGlobal;
+    return nombreCompleto;
 }
 
-function obtenerEdad() {
-    edad = edad + 10
-    return edad
+function obtenerEdad(edad) {
+    edad = edad + 10;
+    return edad;
 }
 
-function verificarMayorEdad() {
-    if(edad >= 18){
-        alert("Puedes pasar" + myVariableGlobal)
+function verificarMayorEdad(edad) {
+    if (edad >= 18) {
+        alert("Puedes pasar " + myVarGlobal);
     } else {
-        alert("Vuelve cuando tengas 18" + myVariableGlobal)
+        alert("Vuelve cuando tengas 18 " + myVarGlobal);
     }
 }
 
 function funcionConReturn() {
-    var myNombreCompleto = obtenerNombreCompleto("carlos", "Medina")
-    var myEdad = obtenerEdad(10)
-    alert("Nombre: " + myNombreCompleto)
-    alert("Edad: " + myEdad)
-    verificarMayorEdad(myEdad)
+    var myNombreCompleto = obtenerNombreCompleto("Carlos", "Vela");
+    var myEdad = obtenerEdad(14);
+    alert("Nombre: " + myNombreCompleto);
+    alert("Edad: " + myEdad);
+    verificarMayorEdad(myEdad);
 }
 
-/* SECCION DE CODIGO PARA LA CALCULADORA*/
+// SECCIÓN DE CÓDIGO PARA LA CALCULADORA
 
-//dar un nuevo valor al INPUT "resultado" en la pàgina html
-function obtenerValor(dig) {
-    document.getElementById("resultado").value = valor
+// Dar un nuevo valor al INPUT "resultado" en la pag HTML.
+function darValor(valor) {
+    document.getElementById("resultado").value = valor;
 }
 
-// obtener el valor actual del INPUT "resultado" en la pag html
+// Obtener el valor actual del INPUT "resultado" en la pag HTML.
 function obtenerValor() {
-    var nro = document.getElementById("resultado").value
-    return nro
-    //alert nro
+    var numero = document.getElementById("resultado").value;
+    return numero;
 }
 
-//adjuntar un nuevo valor a la derecha
-// del INPUT "resultado" en la pag html
+// Adjuntar un nuevo valor a la derecha
+// del INPUT "resultado" en la pag HTML
 function adjuntarValor(numero) {
-    var actualNumero = obtenerValor()
-    var juntarNumeros = actualNumero + "" + numero
-    darValor(juntarNumeros)
+    var actualNumero = obtenerValor();
+    var juntarNumero = actualNumero + "" + numero;
+    darValor(juntarNumero);
 }
 
+function calcularFactorial(N) {
+    var i = 1;
+    var resp = 1;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function factorial(){
-    var N = obtenerValor()
-    var resultadoFact = calcularFactorial(N)
-    darValor(resultadoFact)
-    //alert("Soy Factorial")
-}
-
-function potencia(){
-    alert("Soy Potencia")
-}
-
-function borrar(){
-    var vacio
-    //alert("Soy Borrar")
-}
-
-function dividir(){
-    alert("Soy Dividir")
-}
-
-function numero(dig){
-    adjuntarValor(dig)
-   //alert("Soy Numero " + dig)
-}
-
-function multiplicar(){
-    alert("Soy Multiplicar ")
-}
-
-function restar(){
-    alert("Soy Restar")
-}
-
-function sumar(){
-    alert("Soy Sumar")
-}
-
-function sumar(){
-    alert("Soy Sumar")
-}
-
-function masMenos(){
-    var valorResualtado = obtenerValor()
-    if(valorResualtado == 0){
-        //nada
-    }else{
-        if(valorResualtado > 0){
-            //positivo
-            valorResualtado = valorResualtado * -1
-        }else{
-            //negativo
-            valorResualtado = valorResualtado * -1
+    if (N >= 0) {
+        if (N == 0) {
+            return resp;
+        } else {
+            while (i <= N) {
+                resp *= i;
+                i += 1;
+            }
+            return resp;
         }
+    } else {
+        alert("No existe el factorial de un negativo.");
+        return(N);
     }
-    darValor(valorResualtado)
-    //alert("Soy Mas Menos" + valorResultado)
 }
 
+function factorial() {
+    var actualValor = obtenerValor();
+    darValor(calcularFactorial(actualValor));
+}
+
+
+
+
+
+function potencia() {
+    A = obtenerValor()
+    darValor("")
+    operacion = "potencia"
+    //alert("Soy Potencia");
+}
+
+function borrar() {
+    darValor("");
+}
+
+function dividir() {
+    A = obtenerValor()
+    darValor("")
+    operacion = "dividir"
+    //alert("Soy Dividir");
+}
+
+function multiplicar() {
+    A = obtenerValor()
+    darValor("")
+    operacion = "multiplicar"
+    //alert("Soy Multiplicar");
+}
+
+function restar() {
+    A = obtenerValor()
+    darValor("")
+    operacion = "restar"
+
+    //alert("Soy Restar");
+}
+
+function sumar() {
+    A = obtenerValor()
+    darValor("")
+    operacion = "sumar"
+    //alert("Soy Sumar");
+}
+
+function igual() {
+    var B = obtenerValor()
+    if(operacion == "potencia"){
+        var res = NpotenciaPconParametros(A, B)
+        darValor(res)
+    }
+    if(operacion == "MOD"){
+        var res = A % B
+        darValor(res)
+    }
+    if(operacion == "dividir"){
+        var res = A / B
+        darValor(res)
+    }
+    if(operacion == "multiplicar"){
+        var res = A * B
+        darValor(res)
+    }
+    if(operacion == "restar"){
+        var res = A - B
+        darValor(res)
+    }
+    if(operacion == "sumar"){
+        var res = parseInt(A) + parseInt(B) 
+        darValor(res)
+    }
+
+
+
+
+
+
+   //alert("Soy Igual");
+}
+
+function MOD() {
+    A = obtenerValor()
+    darValor("")
+    operacion = "MOD"
+    //alert("Soy MOD");
+}
+
+function inverso() {
+    var actualValor = obtenerValor();
+    darValor(actualValor*(-1));
+}
+
+function numero(dig) {
+    adjuntarValor(dig);
+}
+
+function NpotenciaP() {
+    var N = parseInt(prompt("Ingrese N:", 3));
+    var P = parseInt(prompt("Ingrese P:", 4));
+    var i = 1 , res = 1
+    while (i <= P) {
+        res = res * N
+        i++
+    }
+    alert(res)
+
+}
+
+function NpotenciaPconParametros(N, P) {
+    var i = 1 , res = 1
+    while (i <= P) {
+        res = res * N
+        i++
+    }
+    return res
+    //alert(res)
+
+}
